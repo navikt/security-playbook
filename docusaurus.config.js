72cd9d4af60a2f97c6abd30b5a7f1a2231e3669f
J-K. Solbakken <jksolbakken@gmail.com>
2022-03-25 12:40:20 +0100

    noen seo-vennlige meta keywords

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,6 +120,12 @@ module.exports = {
       <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       `,
     },
+    metadata: [
+      {
+        name: "keywords",
+        content: "sikkerhet, nav, security champions, playbook, community",
+      },
+    ],
   },
   presets: [
     [
