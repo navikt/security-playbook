e919101c055d128a354b6d32fc233e54fe1a7a00
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2022-06-28 10:28:14 +0200

    Fjern nav-logo fra forsiden

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <img
-          src="https://www.nav.no/dekoratoren/media/nav-logo-red.svg"
-          width="20%"
-        ></img>
+      <div className={styles.headerBox}>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
